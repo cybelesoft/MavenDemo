974c4b5b042ff7c951da56e532b0c83cadd466e6
maugust21 <maugust@cybelesoft.com>
2022-06-03 12:38:59 -0300

    Initial commit

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,48 @@
+/*package example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}*/
+
+package example;		
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+@SuppressWarnings("unused")
+public class NewTest {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("https://192-168-1-129.thinrdp.net:9443");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Thinfinity")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+		    driver = new ChromeDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
