e98500b3a3e98847ee8791a468e8c8068f034f17
maugust21 <maugust@cybelesoft.com>
2022-06-03 17:50:55 -0300

    Update NewTest.java

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -22,6 +22,7 @@ package example;
 
 import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;		
 import org.testng.Assert;		
@@ -29,20 +30,47 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;	
 import org.testng.annotations.AfterTest;		
 @SuppressWarnings("unused")
-public class NewTest {		
+public class NewTest 
+{		
 	    private WebDriver driver;		
-		@Test				
-		public void testEasy() {	
-			driver.get("https://192-168-1-129.thinrdp.net:9443");  
-			String title = driver.getTitle();				 
-			Assert.assertTrue(title.contains("Thinfinity")); 		
-		}	
+		
 		@BeforeTest
-		public void beforeTest() {	
+		public void beforeTest() 
+		{	
 		    driver = new ChromeDriver();  
 		}		
 		@AfterTest
-		public void afterTest() {
+		public void afterTest() 
+		{
 			driver.quit();			
-		}		
+		}
+		@Test				
+		public void testEasy() 
+		{	
+			driver.get("https://192-168-1-129.thinrdp.net:9443");
+			
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Thinfinity")); 		
+		}	
+		@Test
+		public void WindowsLogon_01()
+        {
+            //Arrange (Preparar)
+			driver.navigate().to("https://oficina.thinrdp.net:8443/" + "?signin");
+
+            // //div[@class='main-container']
+			/*driver.WaitUntilIsVisible(By.xpath("//div[@class='main-container']//*[@data-qatest='username_field']")).SendKeys(Config.Windows.Username);
+			driver.WaitUntilIsVisible(By.xpath("//div[@class='main-container']//*[@data-qatest='password_field']")).SendKeys(Config.Windows.Password + Keys.Enter);
+
+
+            //Assert(Afirmar)            
+            String userNameExpected = "martintest";
+            userNameExpected = userNameExpected.toLowerCase();
+
+            WebElement userName = driver.w (By.xpath("//*[@data-qatest='loggedusername_field']"));//_Driver.WaitUntilIsVisible(By.Id("username"));
+            String userNameActual = userNameExpected.toString().toLowerCase();
+            userNameActual = Path.GetFileName(userNameActual);
+*/
+            Assert.assertTrue(false, "");
+        }
 }	
